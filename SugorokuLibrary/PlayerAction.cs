a3ec038466df8c388b542f737c450fcd56ba9829
Ito Haruto <s1701015@sendai-nct.jp>
2020-12-17 14:58:28 +0900

    Add comments, and Add set accessor.

--- SugorokuLibrary/PlayerAction.cs
+++ SugorokuLibrary/PlayerAction.cs
@@ -4,10 +4,15 @@ using System.Text;
 
 namespace SugorokuLibrary
 {
+	/// <summary>
+	/// プレイヤーの行動のクラス
+	/// </summary>
 	public class PlayerAction
 	{
+		///<value>プレイヤーの識別ID</value>
 		public int PlayerID { get; set; }
-		public string ActionType { get; set; }
+
+		///<value>プレイヤーが移動する距離</value>
 		public int Legnth { get; set; }
 
 	}
