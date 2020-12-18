e2cd29872130ed7d1cf94c6ef122fac8677a6ac8
q55mehqs <s1701057@sendai-nct.jp>
2020-12-18 20:51:24 +0900

    merge branch 'dev/q55mehqs' into main

--- SugorokuLibrary/MatchInfo.cs
+++ SugorokuLibrary/MatchInfo.cs
@@ -4,12 +4,27 @@ using System.Text;
 
 namespace SugorokuLibrary
 {
+	/// <summary>
+	/// 試合情報のメタ情報のクラス
+	/// </summary>
 	class MatchInfo
 	{
+		///<value>試合を識別するID</value>
 		public int MatchID { get; set; }
+
+		///<value>プレイヤーIDの配列</value>
 		public int[] PlayerIDs { get; set; }
+
+		///<value>現在のターン数</value>
 		public int Turn { get; set; }
-		public long StartAtUnixTime { get; }
-		public long EndAtUnixTime { get; }
+
+		///<value>試合の開始時間</value>
+		public long StartAtUnixTime { get; set; }
+
+		///<value>試合の終了時間</value>
+		public long EndAtUnixTime { get; set; }
+
+		///<value>次行動するプレイヤーのID</value>
+		public int NextPlayerID { get; set; }
 	}
 }
--- SugorokuLibrary/Player.cs
+++ SugorokuLibrary/Player.cs
@@ -4,10 +4,19 @@ using System.Text;
 
 namespace SugorokuLibrary
 {
+	/// <summary>
+	/// プレイヤーの情報
+	/// </summary>
 	public class Player
 	{
-		public int PlayerID { get; }
-		public int Position { get; }
+		///<value>プレイヤーを識別するためのID</value> 
+		public int PlayerID { get; set; }
+
+		///<value>プレイヤーの名前</value>
+		public int PlayerName { get; set; }
+
+		///<value>プレイヤーの位置</value>
+		public int Position { get; set; }
 
 	}
 }
