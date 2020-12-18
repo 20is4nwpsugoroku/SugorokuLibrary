a5eddfbe7b201b6b2f9f794ffe780df56fb26e41
q55mehqs <s1701057@sendai-nct.jp>
2020-12-18 19:58:48 +0900

    add SugorokuServer

---
+++ SugorokuServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SugorokuLibrary;
+
+namespace SugorokuServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write Server Port
+            var field = new Field();
+            Console.WriteLine(field.Squares[0].Event);
+        }
+    }
+}
