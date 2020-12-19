43e3e55a2b40b7ab67b576669afc72de81e1e2d6
q55mehqs <s1701057@sendai-nct.jp>
2020-12-19 22:05:27 +0900

    update timestamp and create player test

--- SugorokuLibrary/Player.cs
+++ SugorokuLibrary/Player.cs
@@ -1,4 +1,6 @@
-﻿namespace SugorokuLibrary
+﻿using Newtonsoft.Json;
+
+namespace SugorokuLibrary
 {
 	/// <summary>
 	/// プレイヤーの情報
@@ -6,17 +8,30 @@
 	public class Player
 	{
 		///<value>マッチの識別キー</value>
+		[JsonProperty("matchKey")]
 		public string MatchKey { get; set; }
 
-		///<value>プレイヤーを識別するためのID</value> 
+		///<value>プレイヤーを識別するためのID</value>
+		[JsonProperty("playerID")]
 		public int PlayerID { get; set; }
 
 		///<value>プレイヤーの名前</value>
+		[JsonProperty("playerName")]
 		public string PlayerName { get; set; }
 
-		public bool IsHost { get; set; }
+		[JsonProperty("isHost")] public bool IsHost { get; set; }
 
 		///<value>プレイヤーの位置</value>
+		[JsonProperty("position")]
 		public int Position { get; set; }
+
+		public override bool Equals(object? obj)
+		{
+			if (!(obj is Player player)) return false;
+
+			return player.IsHost == IsHost && player.Position == Position &&
+			       player.MatchKey == MatchKey && player.PlayerName == PlayerName &&
+			       player.PlayerID == PlayerID;
+		}
 	}
 }
