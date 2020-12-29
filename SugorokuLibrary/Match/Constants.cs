0446471445afb22b8bfae0fc8763df49c9346ed3
Ito Haruto <s1701015@sendai-nct.jp>
2020-12-30 05:55:51 +0900

    :construction: Add namespace Match Add namespace Match. Add Match.Constants.cs. Add Match.MatchCore.cs.
    Add some constant values in Constants.cs.
    Add some functions in MatchCore.cs.
    
    MatchCore is class of single match.
    It contains information about a single match
    and a function that uses that information to progress the match.

---
+++ SugorokuLibrary/Match/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SugorokuLibrary.Match
+{
+	public static class Constants
+	{
+		public const int ActionMinLength = 0;
+		public const int ActionMaxLength = 7;
+		public const int StartPosition = 0;
+		public const int GoalPosition = 30;
+
+
+	}
+}
---
+++ SugorokuLibrary/Match/MatchCore.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SugorokuLibrary.Match;
+
+namespace SugorokuLibrary.Match
+{
+	public class MatchCore
+	{
+		/// <value> 試合のメタ情報 </value>
+		public MatchInfo MatchInfo { get; set; }
+
+		/// <value> 試合のメタ情報 </value>
+		public Field Field { get; set; }
+
+		/// <value> 試合のメタ情報 </value>
+		public Dictionary<int, Player> Players { get; set; }
+
+
+		/// <value> 行動の順番をPlayerIDの並びで格納する(3順分くらい)</value>
+		private Queue<int> ActionSchedule { get; set; }
+
+
+		private Random Rand { get; set; }
+
+
+		MatchCore()
+		{
+			MatchInfo = new MatchInfo();
+			Field = new Field();
+			Players = new Dictionary<int, Player>();
+			Rand = new Random();
+			ActionSchedule = new Queue<int>();
+		}
+
+
+		/// <summary>
+		/// 試合を開始する処理
+		/// </summary>
+		public void Start()
+		{
+			
+		}
+
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="playerAction">プレイヤーの行動</param>
+		public void ReflectAciton(PlayerAction playerAction)
+		{
+			int prePos, nextPos;
+			int currentPlayerID = MatchInfo.NextPlayerID;
+
+			// エラー処理関連
+			if (playerAction.PlayerID != MatchInfo.NextPlayerID 
+				|| !Players.ContainsKey(playerAction.PlayerID))
+			{
+				return; // 行動できるプレイヤーではないのでリターン
+			}
+
+			if (playerAction.Length <= 0 || playerAction.Length >= 7)
+			{
+				playerAction.Length = Rand.Next(1, 7); // Lengthが不正な場合、さいころをふる
+			}
+
+			// プレイヤーの移動
+			prePos = Players[playerAction.PlayerID].Position;
+			nextPos = prePos + playerAction.Length;
+			if (nextPos >= Constants.GoalPosition)
+			{
+				Goal(currentPlayerID);
+			}
+
+			// イベントの実行
+			Field.Squares[nextPos].Event.Event();
+
+			// 次のターンに進める
+			IncrementTurn();
+		}
+
+
+		/// <summary>
+		/// 行動できるプレイヤーを次に進める処理
+		/// </summary>
+		public void IncrementTurn()
+		{
+			ActionSchedule.Enqueue(ActionSchedule.Dequeue());
+			MatchInfo.NextPlayerID = ActionSchedule.Peek();
+		}
+
+
+		/// <summary>
+		/// プレイヤーがゴールした際の処理
+		/// </summary>
+		/// <param name="playerID">ゴールに到着したプレイヤーのID</param>
+		public void Goal(int playerID)
+		{	
+			// キューから行動の順番を予定を消す処理
+			// プレイヤーの位置をゴール位置に合わせる
+		}
+
+
+		/// <summary>
+		/// 試合を終了させる処理
+		/// </summary>
+		public void End()
+		{
+
+		}
+
+
+		/// <summary>
+		/// 試合終了後、試合結果のみを取得するときの関数
+		/// </summary>
+		public void GetResult()
+		{
+
+		}
+
+
+
+
+	}
+}
