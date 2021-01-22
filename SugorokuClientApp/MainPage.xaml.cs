36e8f07cea83be5cb68af01c21a4058772dcc742
q55mehqs <s1701057@sendai-nct.jp>
2021-01-23 00:31:46 +0900

    update indent from tab to 4 space

--- SugorokuClientApp/MainPage.xaml.cs
+++ SugorokuClientApp/MainPage.xaml.cs
@@ -9,71 +9,71 @@ using Xamarin.Forms;
 
 namespace SugorokuClientApp
 {
-	public partial class MainPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
 
-			LogoImage.Source = ImageSource.FromResource("SugorokuClientApp.ImageResource.logo.png");
-			StartButton.Source = ImageSource.FromResource("SugorokuClientApp.ImageResource.startButton.png");
-		}
+            LogoImage.Source = ImageSource.FromResource("SugorokuClientApp.ImageResource.logo.png");
+            StartButton.Source = ImageSource.FromResource("SugorokuClientApp.ImageResource.startButton.png");
+        }
 
-		private async void OnStartButtonClicked(object sender, EventArgs e)
-		{
-			var serverIpAddress = ServerIpAddress.Text;
-			var serverPort = ServerPort.Text;
-			var playerName = PlayerNameEditor.Text;
-			var roomName = RoomNameEditor.Text;
-			if (string.IsNullOrEmpty(serverIpAddress) || string.IsNullOrEmpty(serverPort) ||
-			    string.IsNullOrEmpty(playerName) || string.IsNullOrEmpty(roomName))
-			{
-				await DisplayAlert("エラー", "サーバーのIPアドレス、ポート番号、プレイヤー名、部屋名のいずれかが不足しています", "OK");
-				return;
-			}
+        private async void OnStartButtonClicked(object sender, EventArgs e)
+        {
+            var serverIpAddress = ServerIpAddress.Text;
+            var serverPort = ServerPort.Text;
+            var playerName = PlayerNameEditor.Text;
+            var roomName = RoomNameEditor.Text;
+            if (string.IsNullOrEmpty(serverIpAddress) || string.IsNullOrEmpty(serverPort) ||
+                string.IsNullOrEmpty(playerName) || string.IsNullOrEmpty(roomName))
+            {
+                await DisplayAlert("エラー", "サーバーのIPアドレス、ポート番号、プレイヤー名、部屋名のいずれかが不足しています", "OK");
+                return;
+            }
 
-			if (!IPAddress.TryParse(serverIpAddress, out var serverIp) || !int.TryParse(serverPort, out var port))
-			{
-				await DisplayAlert("エラー", "IPアドレス、ポート番号の形式が正しくありません", "OK");
-				return;
-			}
+            if (!IPAddress.TryParse(serverIpAddress, out var serverIp) || !int.TryParse(serverPort, out var port))
+            {
+                await DisplayAlert("エラー", "IPアドレス、ポート番号の形式が正しくありません", "OK");
+                return;
+            }
 
-			Application.Current.Properties["serverIpAddress"] = serverIp;
-			Application.Current.Properties["serverPort"] = port;
+            Application.Current.Properties["serverIpAddress"] = serverIp;
+            Application.Current.Properties["serverPort"] = port;
 
-			Player playerData = null;
-			try
-			{
-				using var socket = ConnectServer.CreateSocket(serverIp, port);
-				var createPlayerMessage = new CreatePlayerMessage(playerName, roomName);
-				var msg = JsonConvert.SerializeObject(createPlayerMessage);
-				var (_, result, recvMsg) = Connection.SendAndRecvMessage(msg, socket, true);
+            Player playerData = null;
+            try
+            {
+                using var socket = ConnectServer.CreateSocket(serverIp, port);
+                var createPlayerMessage = new CreatePlayerMessage(playerName, roomName);
+                var msg = JsonConvert.SerializeObject(createPlayerMessage);
+                var (_, result, recvMsg) = Connection.SendAndRecvMessage(msg, socket, true);
 
-				if (!result)
-				{
-					var failMessage = JsonConvert.DeserializeObject<FailedMessage>(recvMsg);
-					await DisplayAlert("部屋の作成に失敗", failMessage.Message, "OK");
-					return;
-				}
+                if (!result)
+                {
+                    var failMessage = JsonConvert.DeserializeObject<FailedMessage>(recvMsg);
+                    await DisplayAlert("部屋の作成に失敗", failMessage.Message, "OK");
+                    return;
+                }
 
-				playerData = JsonConvert.DeserializeObject<Player>(recvMsg);
-				// Application.Current.Properties["playerData"] = playerData;
-			}
-			catch (Exception exception)
-			{
-				Console.WriteLine(exception);
-				await DisplayAlert("エラー", "ソケットのConnectで例外が発生しました。やり直してください。", "OK");
-			}
-			finally
-			{
-				if (playerData != null)
-				{
-					Device.BeginInvokeOnMainThread(async () =>
-					{
-						await Navigation.PushAsync(new WaitOtherPlayerPage(playerData));
-					});
-				}
-			}
-		}
-	}
+                playerData = JsonConvert.DeserializeObject<Player>(recvMsg);
+                // Application.Current.Properties["playerData"] = playerData;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                await DisplayAlert("エラー", "ソケットのConnectで例外が発生しました。やり直してください。", "OK");
+            }
+            finally
+            {
+                if (playerData != null)
+                {
+                    Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        await Navigation.PushAsync(new WaitOtherPlayerPage(playerData));
+                    });
+                }
+            }
+        }
+    }
 }
--- SugorokuClientApp/WaitOtherPlayerPage.xaml.cs
+++ SugorokuClientApp/WaitOtherPlayerPage.xaml.cs
@@ -10,79 +10,79 @@ using Xamarin.Forms.Xaml;
 
 namespace SugorokuClientApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class WaitOtherPlayerPage
-	{
-		private readonly Player _myPlayerInfo;
-		private bool _hostPlayersPageMoved;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WaitOtherPlayerPage
+    {
+        private readonly Player _myPlayerInfo;
+        private bool _hostPlayersPageMoved;
 
-		public WaitOtherPlayerPage(Player myInfo)
-		{
-			InitializeComponent();
+        public WaitOtherPlayerPage(Player myInfo)
+        {
+            InitializeComponent();
 
-			_myPlayerInfo = myInfo;
-			if (myInfo.IsHost)
-			{
-				DisplayAlert("ホストになりました", "あなたは部屋のホストになりました。プレイヤーが集まったら「ゲーム開始」をタップしてください", "OK");
-				GameStartButton.IsEnabled = true;
-			}
-			else
-			{
-				DisplayAlert("あなたは参加者です", $"あなたは部屋{myInfo.MatchKey}に参加しました。ホストの開始までしばらくお待ちください。", "OK");
-				GameStartButton.IsEnabled = false;
-			}
+            _myPlayerInfo = myInfo;
+            if (myInfo.IsHost)
+            {
+                DisplayAlert("ホストになりました", "あなたは部屋のホストになりました。プレイヤーが集まったら「ゲーム開始」をタップしてください", "OK");
+                GameStartButton.IsEnabled = true;
+            }
+            else
+            {
+                DisplayAlert("あなたは参加者です", $"あなたは部屋{myInfo.MatchKey}に参加しました。ホストの開始までしばらくお待ちください。", "OK");
+                GameStartButton.IsEnabled = false;
+            }
 
-			PlayersView.ItemsSource = new ListQueue<Player> {myInfo};
+            PlayersView.ItemsSource = new ListQueue<Player> {myInfo};
 
-			Device.StartTimer(TimeSpan.FromSeconds(5), PlayerInfoUpdate);
-		}
+            Device.StartTimer(TimeSpan.FromSeconds(5), PlayerInfoUpdate);
+        }
 
-		private bool PlayerInfoUpdate()
-		{
-			using var socket = ConnectServer.CreateSocket((IPAddress) Application.Current.Properties["serverIpAddress"],
-				(int) Application.Current.Properties["serverPort"]);
-			var requestMethod = new GetMatchInfoMessage(_myPlayerInfo.MatchKey);
-			var requestText = JsonConvert.SerializeObject(requestMethod);
-			var (_, result, msg) = Connection.SendAndRecvMessage(requestText, socket, true);
+        private bool PlayerInfoUpdate()
+        {
+            using var socket = ConnectServer.CreateSocket((IPAddress) Application.Current.Properties["serverIpAddress"],
+                (int) Application.Current.Properties["serverPort"]);
+            var requestMethod = new GetMatchInfoMessage(_myPlayerInfo.MatchKey);
+            var requestText = JsonConvert.SerializeObject(requestMethod);
+            var (_, result, msg) = Connection.SendAndRecvMessage(requestText, socket, true);
 
-			if (!result)
-			{
-				var failed = JsonConvert.DeserializeObject<FailedMessage>(msg);
-				throw new Exception($"MatchKey Error: {failed.Message}");
-			}
+            if (!result)
+            {
+                var failed = JsonConvert.DeserializeObject<FailedMessage>(msg);
+                throw new Exception($"MatchKey Error: {failed.Message}");
+            }
 
-			if (_hostPlayersPageMoved) return false;
+            if (_hostPlayersPageMoved) return false;
 
-			var matchInfo = JsonConvert.DeserializeObject<MatchInfo>(msg);
-			if (matchInfo.CreatePlayerClosed)
-			{
-				Device.BeginInvokeOnMainThread(async () =>
-				{
-					await Navigation.PushAsync(new PlayPage(_myPlayerInfo));
-				});
-				return false;
-			}
+            var matchInfo = JsonConvert.DeserializeObject<MatchInfo>(msg);
+            if (matchInfo.CreatePlayerClosed)
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PushAsync(new PlayPage(_myPlayerInfo));
+                });
+                return false;
+            }
 
-			PlayersView.ItemsSource = matchInfo.Players;
-			return true;
-		}
+            PlayersView.ItemsSource = matchInfo.Players;
+            return true;
+        }
 
-		private async void GameStartButtonClicked(object sender, EventArgs e)
-		{
-			using var socket = ConnectServer.CreateSocket((IPAddress) Application.Current.Properties["serverIpAddress"],
-				(int) Application.Current.Properties["serverPort"]);
-			var requestMethod = new CloseCreateMessage(_myPlayerInfo.MatchKey);
-			var requestText = JsonConvert.SerializeObject(requestMethod);
-			var (_, result, msg) = Connection.SendAndRecvMessage(requestText, socket, true);
+        private async void GameStartButtonClicked(object sender, EventArgs e)
+        {
+            using var socket = ConnectServer.CreateSocket((IPAddress) Application.Current.Properties["serverIpAddress"],
+                (int) Application.Current.Properties["serverPort"]);
+            var requestMethod = new CloseCreateMessage(_myPlayerInfo.MatchKey);
+            var requestText = JsonConvert.SerializeObject(requestMethod);
+            var (_, result, msg) = Connection.SendAndRecvMessage(requestText, socket, true);
 
-			if (!result)
-			{
-				var failed = JsonConvert.DeserializeObject<FailedMessage>(msg);
-				throw new Exception($"CloseCreate Error: {failed.Message}");
-			}
+            if (!result)
+            {
+                var failed = JsonConvert.DeserializeObject<FailedMessage>(msg);
+                throw new Exception($"CloseCreate Error: {failed.Message}");
+            }
 
-			_hostPlayersPageMoved = true;
-			await Navigation.PushAsync(new PlayPage(_myPlayerInfo));
-		}
-	}
+            _hostPlayersPageMoved = true;
+            await Navigation.PushAsync(new PlayPage(_myPlayerInfo));
+        }
+    }
 }
