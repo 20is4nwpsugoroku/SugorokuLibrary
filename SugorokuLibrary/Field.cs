e5c42790de4cd2dc500d3d3498e105c238265a47
q55mehqs <s1701057@sendai-nct.jp>
2021-01-23 16:01:16 +0900

    fix parse square (calls only one)

--- SugorokuLibrary/Field.cs
+++ SugorokuLibrary/Field.cs
@@ -1,47 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Reflection;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using SugorokuLibrary.SquareEvents;
 
 namespace SugorokuLibrary
 {
-	public class Field
-	{
-		public static Square[] Squares => ParseSquares();
+    public class Field
+    {
+        public SquareEvent[] Squares { get; }
 
-		private static Square[] ParseSquares()
-		{
-			var asm = Assembly.GetExecutingAssembly();
-			using var stream = asm.GetManifestResourceStream("SugorokuLibrary.squareData.json");
-			using var reader = new StreamReader(stream!);
+        public Field()
+        {
+            Squares = ParseSquares();
+        }
 
-			var jsonString = reader.ReadToEnd();
+        private static SquareEvent[] ParseSquares()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            using var stream = asm.GetManifestResourceStream("SugorokuLibrary.squareData.json");
+            using var reader = new StreamReader(stream!);
 
-			var squares = JsonConvert.DeserializeObject<IEnumerable<Dictionary<string, JObject>>>(jsonString);
+            var jsonString = reader.ReadToEnd();
 
-			return squares!.Select(s =>
-			{
-				var square = new Square {Index = (int) s["index"]};
-
-				ISquareEvent e = (string) s["eventType"]! switch
-				{
-					"none" => new NoneEvent(),
-					"prev" => new PrevEvent {BackCount = (int) s["count"]},
-					"next" => new NextEvent {NextCount = (int) s["count"]},
-					"prevDice" => new PrevDiceEvent(),
-					"wait" => new WaitEvent(),
-					"goStart" => new GoStartEvent(),
-					"diceAgain" => new DiceAgainEvent(),
-					_ => throw new ArgumentOutOfRangeException()
-				};
-
-				square.Event = e;
-				return square;
-			}).ToArray();
-		}
-	}
+            return JsonConvert.DeserializeObject<SquareEvent[]>(jsonString);
+        }
+    }
 }
