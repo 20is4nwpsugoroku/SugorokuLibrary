e25c0a22e200a986ebc1a34ac25535c81805b735
q55mehqs <s1701057@sendai-nct.jp>
2021-01-24 01:15:19 +0900

    update from NotImplementedException to NotSupportedException

--- SugorokuLibrary/SquareEvents/Converter/SquareEventConverter.cs
+++ SugorokuLibrary/SquareEvents/Converter/SquareEventConverter.cs
@@ -8,7 +8,7 @@ namespace SugorokuLibrary.SquareEvents.Converter
     {
         public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
