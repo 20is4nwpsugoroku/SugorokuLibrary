9d7f911a9fa1a786a286006ba0d6c9f8a8618afb
q55mehqs <s1701057@sendai-nct.jp>
2021-01-23 23:25:32 +0900

    move saikoro to Android resource (3)

--- SugorokuClientApp.Android/Resources/Resource.designer.cs
+++ SugorokuClientApp.Android/Resources/Resource.designer.cs
@@ -11228,13 +11228,31 @@ namespace SugorokuClientApp.Droid
 			public const int notify_panel_notification_icon_bg = 2131165324;
 			
 			// aapt resource value: 0x7F07008D
-			public const int test_custom_background = 2131165325;
+			public const int saikoro_1 = 2131165325;
 			
 			// aapt resource value: 0x7F07008E
-			public const int tooltip_frame_dark = 2131165326;
+			public const int saikoro_2 = 2131165326;
 			
 			// aapt resource value: 0x7F07008F
-			public const int tooltip_frame_light = 2131165327;
+			public const int saikoro_3 = 2131165327;
+			
+			// aapt resource value: 0x7F070090
+			public const int saikoro_4 = 2131165328;
+			
+			// aapt resource value: 0x7F070091
+			public const int saikoro_5 = 2131165329;
+			
+			// aapt resource value: 0x7F070092
+			public const int saikoro_6 = 2131165330;
+			
+			// aapt resource value: 0x7F070093
+			public const int test_custom_background = 2131165331;
+			
+			// aapt resource value: 0x7F070094
+			public const int tooltip_frame_dark = 2131165332;
+			
+			// aapt resource value: 0x7F070095
+			public const int tooltip_frame_light = 2131165333;
 			
 			static Drawable()
 			{
