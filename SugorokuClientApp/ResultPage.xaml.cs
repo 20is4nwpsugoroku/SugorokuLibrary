2ec47603bae38fbe58de175b989523f44cd2f199
q55mehqs <s1701057@sendai-nct.jp>
2021-01-24 16:00:42 +0900

    update Result Page

--- SugorokuClientApp/ResultPage.xaml.cs
+++ SugorokuClientApp/ResultPage.xaml.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using SugorokuLibrary;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -6,11 +8,31 @@ using Xamarin.Forms.Xaml;
 namespace SugorokuClientApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ResultPage : ContentPage
+    public partial class ResultPage
     {
-        public ResultPage(IEnumerable<int> ranking, int myId, IEnumerable<Player> players)
+        public ResultPage(IEnumerable<int> ranking, IReadOnlyCollection<Player> players)
         {
             InitializeComponent();
+
+            var resultViewModels = new List<ResultPageViewModel>();
+
+            foreach (var (rank, id) in ranking.Select((playerId, i) => (i, playerId)))
+            {
+                var player = players.First(p => p.PlayerID == id);
+                var playerViewModel = new ResultPageViewModel
+                {
+                    PlayerName = player.PlayerName, RankText = $"{rank + 1}位",
+                    ImageSource = ImageSource.FromResource($"SugorokuClientApp.ImageResource.koma_{id}.png")
+                };
+                resultViewModels.Add(playerViewModel);
+            }
+
+            ResultView.ItemsSource = resultViewModels;
+        }
+
+        private async void GameFinishButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync(false);
         }
     }
 }
